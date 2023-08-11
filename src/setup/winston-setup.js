128aac234738178ba0a90bcc750ab5f67c6d83b7
kong-droid <rhdalgid134@gmail.com>
2023-08-11 11:42:05 +0900

    fix winston config

--- src/setup/winston-setup.js
+++ src/setup/winston-setup.js
@@ -1,37 +1,32 @@
-import  *  as  winston  from  'winston';
-import  'winston-daily-rotate-file';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
 import envSetup from "./env-setup.js";
 
 envSetup();
-const winstonSetup = () => {
 
-  const transport = new winston.transports.DailyRotateFile({
-    filename: 'application-%DATE%.log',
-    datePattern: 'YYYY-MM-DD-HH',
-    zippedArchive: true,
-    maxSize: '20m',
-    maxFiles: '14d'
-  }).on('rotate', (oldFilename, newFilename) => {
-    // do something fun
-  });
+const transport = new winston.transports.DailyRotateFile({
+  level: process.env.NODE_ENV_LOG_LEVEL,
+  datePattern: 'YYYY-MM-DD',
+  dirname: process.env.NODE_ENV_LOG_FILE_DIR,
+  filename: `belogger-cron_%DATE%.log`,
+  maxFiles: 100,         // after 100 day delete..
+  zippedArchive: false,  // zipping log files
+});
 
-  const consoleOpts = {
-    handleExceptions: true,
-    level: 'info',
-    format: combine(
-        colorize({ all: true }),
-        timestamp({ format: 'YYYY-MM-DD HH:mm:ss:ms' })
-    )
-  }
+const {combine, timestamp, printf} = winston.format;
 
-  const logger = winston.createLogger({
-    level: ,
-    format: winston.format.json(),
-    transports: [
-      transport
-    ]
-  });
+const logFormat = printf(f => {
+  return `${f.timestamp} ${f.level}: ${f.message}`;
+});
 
-};
-
-export default winstonSetup;
+export const log = winston.createLogger({
+  level: process.env.NODE_ENV_LOG_LEVEL_MESSAGE,
+  format: combine(
+      timestamp({format: 'YYYY-MM-DD HH:mm:ss'}),
+      logFormat,
+      winston.format.colorize()
+  ),
+  transports: [
+    transport
+  ]
+});
--- src/utils/date-util.js
+++ src/utils/date-util.js
@@ -2,7 +2,6 @@ const current = new Date();
 
 // first Date
 const firstDate = new Date(current.getFullYear(), current.getMonth(), 1);
-const formattedFirstDate = formatDate(firstDate);
 
 // next Month
 const nextMonth = current.getMonth() + 1;
@@ -10,7 +9,7 @@ const nextMonthFirstDate = new Date(current.getFullYear(), nextMonth, 1);
 
 // last Date
 const lastDate = new Date(nextMonthFirstDate.getTime() - 1);
-const formattedLastDate = formatDate(lastDate);
+
 const formatDate = (date) => {
   const year = date.getFullYear();
   const month  = String(date.getMonth() + 1).padStart(2, '0');
@@ -18,7 +17,11 @@ const formatDate = (date) => {
   return `${year}-${month}-${day}`;
 }
 
-// export function
+// formatted Date
+const formattedFirstDate = formatDate(firstDate);
+const formattedLastDate = formatDate(lastDate);
+
+// export date return function
 export const dateParams = () => {
   return {
     firstDate: formattedFirstDate,
